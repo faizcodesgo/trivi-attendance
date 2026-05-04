477c183820e302a69f5db7735458c61582387a1d
Faiz <faiz.codes.go@gmail.com>
2026-05-04 20:47:47 +0530

    fixed password conflict

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const allowedUsers = require("./allowedUsers");
@@ -11,7 +10,6 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-
       const email = profile.emails[0].value;
 
       if (!allowedUsers.includes(email)) {
@@ -24,31 +22,5 @@ passport.use(
 );
 
 passport.serializeUser((user, done) => done(null, user));
-=======
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const allowedUsers = require("./allowedUsers");
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-
-      const email = profile.emails[0].value;
-
-      if (!allowedUsers.includes(email)) {
-        return done(null, false);
-      }
-
-      return done(null, profile);
-    }
-  )
-);
-
-passport.serializeUser((user, done) => done(null, user));
->>>>>>> dd49053 (initial commit)
 passport.deserializeUser((user, done) => done(null, user));
