51714e202b04e201b0d2e7c28df57d67067198db
Faiz <faiz.codes.go@gmail.com>
2026-05-04 20:55:16 +0530

    fixed allowedUsers conflict

--- config/allowedUsers.js
+++ config/allowedUsers.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 module.exports = [
   "faiz.codes.go@gmail.com",
   "faizatrivi@gmail.com"
-=======
-module.exports = [
-  "faiz.codes.go@gmail.com",
-  "faizatrivi@gmail.com"
->>>>>>> dd49053 (initial commit)
 ];
