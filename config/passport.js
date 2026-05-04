402b00c177f985c92e13362187f36096a21c5a7d
Faiz <faiz.codes.go@gmail.com>
2026-05-04 23:41:38 +0530

    fixed https callback url

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://trivi-attendance.onrender.com/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       const email = profile.emails[0].value;
